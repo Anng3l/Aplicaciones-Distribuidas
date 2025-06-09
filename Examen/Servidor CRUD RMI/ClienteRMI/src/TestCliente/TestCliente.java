cda8828f22c75cc897f8ff005bf67fe9ea3e95fe
Angel <151583275+Anng3l@users.noreply.github.com>
2025-06-09 11:52:22 -0500

    Lp

--- Examen/Servidor CRUD RMI/ClienteRMI/src/TestCliente/TestCliente.java
+++ Examen/Servidor CRUD RMI/ClienteRMI/src/TestCliente/TestCliente.java
@@ -9,7 +9,7 @@ import java.rmi.Naming;
 
 public class TestCliente {
     public static void main(String[] args) throws Exception {
-        String rmiObjectName = "rmi://localhost/Datos";
+        String rmiObjectName = "rmi://192.168.100.21:1099/Datos";
         Servidor servicio = null;
         servicio = (Servidor) Naming.lookup(rmiObjectName);
         new VentanaCarrera(servicio);
---
+++ Examen/Servidor RMI -- Examen/src/clase/Persona.java
@@ -0,0 +1,46 @@
+package clase;
+
+import lombok.Getter;
+
+import java.io.Serializable;
+
+public class Persona implements Serializable {
+
+    //Los mismo en ambos lados
+    private static final long serialVersionUID = 1L;
+
+    @Getter private int clave;
+    @Getter private String nombre;
+    @Getter private String correo;
+    @Getter private String cargo;
+    @Getter private double sueldo;
+
+    public Persona(int clave, String nombre, String correo, String cargo, double sueldo) {
+        this.clave = clave;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.cargo = cargo;
+        this.sueldo = sueldo;
+    }
+
+
+    public int getClave() {
+        return clave;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+}
